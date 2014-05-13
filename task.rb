88bd6bd7d4f673742a169019f8268c706d6eb896
Adrian Pask & Shaun McGeever <pair+adrian-pask.shaunmcgeever@devbootcamp.com>
2014-05-13 15:48:50 -0400

    removed all code from models

--- task.rb
+++ task.rb
@@ -1,17 +1,3 @@
 class Task
-  attr_accessor :title, :description, :status, :created_at
-
-  def initialize
-    @status = "incomplete"
-    @created_at = DateTime.now
-  end
-
-  # def mark_as_complete!
-  #   @status = "complete"
-  # end
-
-  # def mark_as_incomplete!
-
-  # end
 
 end
--- todo.rb
+++ todo.rb
@@ -1,16 +1,3 @@
 class Todo
-  #has_many :tasks
 
-  def initialize
-    @tasks = []
-  end
-
-  def add_task(task)
-    if task.kind_of? Task
-        @tasks << task
-    else
-      raise 100
-    end
-
-  end
 end
